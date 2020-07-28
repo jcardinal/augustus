7e75bb827bfe5b0dba7a8164d1d6f7916df0e4ec
Justin Cardinal <justin@somuchuptime.com>
2020-07-28 15:54:09 -0500

    Let's rename this thing!

--- augustus/asgi.py
+++ augustus/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vocablog project.
+ASGI config for augustus project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vocablog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'augustus.settings')
 
 application = get_asgi_application()
--- augustus/wsgi.py
+++ augustus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vocablog project.
+WSGI config for augustus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vocablog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'augustus.settings')
 
 application = get_wsgi_application()
