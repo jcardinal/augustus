c9b4f7fdefb212425ac3c87a8adb1997386e720a
Justin Cardinal <justin@somuchuptime.com>
2020-08-18 08:14:02 -0500

    Upgrade to django 3.1, use new built-in JSONField support

--- vocab/models.py
+++ vocab/models.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
 class Word(models.Model):
     word = models.CharField(max_length=50)
-    data = JSONField()
+    data = models.JSONField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
