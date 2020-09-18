3c28271a99e152395c9a425cd7368ba45b64622a
Justin Cardinal <justin@somuchuptime.com>
2020-09-18 17:07:30 -0500

    Remove redundant arg, already implied by default required=True

--- vocab/forms.py
+++ vocab/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class AddExampleForm(forms.Form):
-    text = forms.CharField(max_length=2048, min_length=1)
+    text = forms.CharField(max_length=2048)
     source = forms.URLField()
