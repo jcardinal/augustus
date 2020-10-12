9d051a1f235acb16d2a2b373a869170c90ce7d51
Justin Cardinal <justin@somuchuptime.com>
2020-10-12 14:45:07 -0500

    Create user login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.urls import reverse
 from .forms import UserRegisterForm
 
 def register(request):
@@ -9,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect(reverse('word', kwargs={'word': 'success'}))
+            messages.success(request, f'Account created for {username}. You can now log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
