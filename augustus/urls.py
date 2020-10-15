387d81d268d7e16a97b570514c8b5af2f7759731
Justin Cardinal <justin@somuchuptime.com>
2020-10-15 14:26:07 -0500

    Create profile page, require login for profile and word pages

--- augustus/urls.py
+++ augustus/urls.py
@@ -21,6 +21,7 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accts/register/', users_views.register, name='register'),
+    path('accts/profile/', users_views.profile, name='profile'),
     path('accts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('accts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include("vocab.urls")),
