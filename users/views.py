36e9138e2eba289c19567f8deefa3422ea5a6ef6
Justin Cardinal <justin@somuchuptime.com>
2021-02-08 13:00:18 -0600

    Create flag for disabling user registration
    Update template to explain when registration is disabled.
    Prevent form submissions when registration is disabled.

--- users/views.py
+++ users/views.py
@@ -1,20 +1,32 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+import os
+
+REG_DISABLED = os.environ.get("DJANGO_REG_DISABLED", "False") == "True"
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
+        if REG_DISABLED:
+            return HttpResponse(status=403)
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}. You can now log in.')
-            return redirect('login')
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Account created for {username}. You can now log in."
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(
+        request, "users/register.html", {"form": form, "reg_disabled": REG_DISABLED}
+    )
+
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    return render(request, "users/profile.html")
